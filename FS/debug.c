e46dcc57603f7864b2750ecb9fd2ecb8208ce7c4
michalsc <michalsc>
2005-11-14 22:46:41 +0000

    added support for AROS debug output

--- FS/debug.c
+++ FS/debug.c
@@ -11,11 +11,14 @@
 #include "fs.h"
 
 extern ULONG mask_debug;
-extern void __asm TOSERIAL(register __a0 UBYTE *);
 
 /* Internal prototypes */
 
 #ifdef DEBUGCODE
+
+#ifndef __AROS__
+    extern void __asm TOSERIAL(register __a0 UBYTE *);
+#endif
 
 struct EClockVal ecv;
 
@@ -40,166 +43,169 @@ void END(UBYTE *name) {
   DEBUG(("%s: ticks %ld (f=%ld)\n",name,diff,freq));
 }
 
-#ifndef DEBUGKPRINTF
+#ifndef __AROS__
 
-  #ifndef DEBUG115200
-
-    void debug(UBYTE *fmt, ... ) {
-      struct MsgPort *port;
-      struct Message *msg;
-      UBYTE *dest;
-      ULONG *args;
-
-      Forbid();
-      if((port=FindPort("SFS debug output port"))!=0) {
-        if((msg=AllocVec(sizeof(struct Message)+512,MEMF_CLEAR))!=0) {
-          dest=(UBYTE *)msg;
-          dest+=sizeof(struct Message);
-          args=(ULONG *)&fmt;
-          args++;
-
-          RawDoFmt(fmt,args,(void (*)())"\x16\xC0\x4E\x75",dest);
-
-          PutMsg(port,msg);
+    #ifndef DEBUGKPRINTF
+    
+      #ifndef DEBUG115200
+    
+        void debug(UBYTE *fmt, ... ) {
+          struct MsgPort *port;
+          struct Message *msg;
+          UBYTE *dest;
+          ULONG *args;
+    
+          Forbid();
+          if((port=FindPort("SFS debug output port"))!=0) {
+            if((msg=AllocVec(sizeof(struct Message)+512,MEMF_CLEAR))!=0) {
+              dest=(UBYTE *)msg;
+              dest+=sizeof(struct Message);
+              args=(ULONG *)&fmt;
+              args++;
+    
+              RawDoFmt(fmt,args,(void (*)())"\x16\xC0\x4E\x75",dest);
+    
+              PutMsg(port,msg);
+            }
+          }
+          Permit();
         }
-      }
-      Permit();
-    }
-
-
-
-    void tdebug(UBYTE *fmt, ... ) {
-      struct MsgPort *port;
-      struct Message *msg;
-      struct DateStamp ds;
-      UBYTE *dest;
-      ULONG *args;
-
-      DateStamp(&ds);
-
-      debug("%4ld.%4ld ", ds.ds_Minute, ds.ds_Tick*2);
-
-      Forbid();
-      if((port=FindPort("SFS debug output port"))!=0) {
-        if((msg=AllocVec(sizeof(struct Message)+512,MEMF_CLEAR))!=0) {
-          dest=(UBYTE *)msg;
-          dest+=sizeof(struct Message);
-          args=(ULONG *)&fmt;
-          args++;
-
-          RawDoFmt(fmt,args,(void (*)())"\x16\xC0\x4E\x75",dest);
-
-          PutMsg(port,msg);
+    
+    
+    
+        void tdebug(UBYTE *fmt, ... ) {
+          struct MsgPort *port;
+          struct Message *msg;
+          struct DateStamp ds;
+          UBYTE *dest;
+          ULONG *args;
+    
+          DateStamp(&ds);
+    
+          debug("%4ld.%4ld ", ds.ds_Minute, ds.ds_Tick*2);
+    
+          Forbid();
+          if((port=FindPort("SFS debug output port"))!=0) {
+            if((msg=AllocVec(sizeof(struct Message)+512,MEMF_CLEAR))!=0) {
+              dest=(UBYTE *)msg;
+              dest+=sizeof(struct Message);
+              args=(ULONG *)&fmt;
+              args++;
+    
+              RawDoFmt(fmt,args,(void (*)())"\x16\xC0\x4E\x75",dest);
+    
+              PutMsg(port,msg);
+            }
+          }
+          Permit();
         }
-      }
-      Permit();
-    }
-
-
-
-    void xdebug(ULONG type,UBYTE *fmt, ... ) {
-      struct MsgPort *port;
-      struct Message *msg;
-      UBYTE *dest;
-      ULONG *args;
-    //  ULONG debug=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_IO-DEBUG_SEEK-DEBUG_LOCK-DEBUG_BITMAP;
-    //  ULONG debugdetailed=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_IO-DEBUG_SEEK-DEBUG_LOCK-DEBUG_BITMAP;
-    //  ULONG debug=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_LOCK-DEBUG_BITMAP;
-    //  ULONG debugdetailed=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_LOCK-DEBUG_BITMAP;
-    //  ULONG debug=0xFFFFFFFE;
-    //  ULONG debugdetailed=0xFFFFFFFE;
-      ULONG debug=mask_debug;
-      ULONG debugdetailed=mask_debug & ~(DEBUG_CACHEBUFFER|DEBUG_NODES|DEBUG_LOCK|DEBUG_BITMAP);
-
-      if((debugdetailed & type)!=0 || ((type & 1)==0 && (debug & type)!=0)) {
-        Forbid();
-        if((port=FindPort("SFS debug output port"))!=0) {
-          if((msg=AllocVec(sizeof(struct Message)+512,MEMF_CLEAR))!=0) {
-            dest=(UBYTE *)msg;
-            dest+=sizeof(struct Message);
-            args=(ULONG *)&fmt;
-            args++;
-
-            RawDoFmt(fmt,args,(void (*)())"\x16\xC0\x4E\x75",dest);
-
-            PutMsg(port,msg);
+    
+    
+    
+        void xdebug(ULONG type,UBYTE *fmt, ... ) {
+          struct MsgPort *port;
+          struct Message *msg;
+          UBYTE *dest;
+          ULONG *args;
+        //  ULONG debug=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_IO-DEBUG_SEEK-DEBUG_LOCK-DEBUG_BITMAP;
+        //  ULONG debugdetailed=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_IO-DEBUG_SEEK-DEBUG_LOCK-DEBUG_BITMAP;
+        //  ULONG debug=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_LOCK-DEBUG_BITMAP;
+        //  ULONG debugdetailed=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_LOCK-DEBUG_BITMAP;
+        //  ULONG debug=0xFFFFFFFE;
+        //  ULONG debugdetailed=0xFFFFFFFE;
+          ULONG debug=mask_debug;
+          ULONG debugdetailed=mask_debug & ~(DEBUG_CACHEBUFFER|DEBUG_NODES|DEBUG_LOCK|DEBUG_BITMAP);
+    
+          if((debugdetailed & type)!=0 || ((type & 1)==0 && (debug & type)!=0)) {
+            Forbid();
+            if((port=FindPort("SFS debug output port"))!=0) {
+              if((msg=AllocVec(sizeof(struct Message)+512,MEMF_CLEAR))!=0) {
+                dest=(UBYTE *)msg;
+                dest+=sizeof(struct Message);
+                args=(ULONG *)&fmt;
+                args++;
+    
+                RawDoFmt(fmt,args,(void (*)())"\x16\xC0\x4E\x75",dest);
+    
+                PutMsg(port,msg);
+              }
+            }
+            Permit();
           }
         }
-        Permit();
-      }
-    }
-
-  #else
-
-    UBYTE serbuffer[500];
-
-    void debug(UBYTE *fmt, ... ) {
+    
+      #else
+    
+        UBYTE serbuffer[500];
+    
+        void debug(UBYTE *fmt, ... ) {
+          ULONG *args;
+    
+          args=(ULONG *)&fmt;
+          args++;
+    
+          RawDoFmt(fmt,args,(void (*)())"\x16\xC0\x4E\x75",serbuffer);
+    
+          Disable();
+    
+          TOSERIAL(serbuffer);
+    
+          Enable();
+        }
+    
+    
+    
+        void xdebug(ULONG type,UBYTE *fmt, ... ) {
+          ULONG *args;
+        //  ULONG debug=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_IO-DEBUG_SEEK-DEBUG_LOCK-DEBUG_BITMAP;
+        //  ULONG debugdetailed=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_IO-DEBUG_SEEK-DEBUG_LOCK-DEBUG_BITMAP;
+        //  ULONG debug=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_LOCK-DEBUG_BITMAP;
+        //  ULONG debugdetailed=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_LOCK-DEBUG_BITMAP;
+        //  ULONG debug=0xFFFFFFFE;
+        //  ULONG debugdetailed=0xFFFFFFFE;
+          ULONG debug=mask_debug;
+          ULONG debugdetailed=mask_debug & ~(DEBUG_CACHEBUFFER|DEBUG_NODES|DEBUG_LOCK|DEBUG_BITMAP);
+    
+          if((debugdetailed & type)!=0 || ((type & 1)==0 && (debug & type)!=0)) {
+    
+            args=(ULONG *)&fmt;
+            args++;
+    
+            RawDoFmt(fmt,args,(void (*)())"\x16\xC0\x4E\x75",serbuffer);
+    
+            Disable();
+    
+            TOSERIAL(serbuffer);
+    
+            Enable();
+          }
+        }
+    
+      #endif
+    
+    
+    #else
+    
+    void xkprintf(ULONG type,const char *fmt, ... ) {
+      UBYTE *str;
+      UBYTE *dest;
       ULONG *args;
-
-      args=(ULONG *)&fmt;
-      args++;
-
-      RawDoFmt(fmt,args,(void (*)())"\x16\xC0\x4E\x75",serbuffer);
-
-      Disable();
-
-      TOSERIAL(serbuffer);
-
-      Enable();
-    }
-
-
-
-    void xdebug(ULONG type,UBYTE *fmt, ... ) {
-      ULONG *args;
-    //  ULONG debug=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_IO-DEBUG_SEEK-DEBUG_LOCK-DEBUG_BITMAP;
-    //  ULONG debugdetailed=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_IO-DEBUG_SEEK-DEBUG_LOCK-DEBUG_BITMAP;
-    //  ULONG debug=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_LOCK-DEBUG_BITMAP;
-    //  ULONG debugdetailed=0xFFFFFFFE-DEBUG_CACHEBUFFER-DEBUG_NODES-DEBUG_LOCK-DEBUG_BITMAP;
-    //  ULONG debug=0xFFFFFFFE;
-    //  ULONG debugdetailed=0xFFFFFFFE;
       ULONG debug=mask_debug;
       ULONG debugdetailed=mask_debug & ~(DEBUG_CACHEBUFFER|DEBUG_NODES|DEBUG_LOCK|DEBUG_BITMAP);
-
+    
       if((debugdetailed & type)!=0 || ((type & 1)==0 && (debug & type)!=0)) {
-
-        args=(ULONG *)&fmt;
-        args++;
-
-        RawDoFmt(fmt,args,(void (*)())"\x16\xC0\x4E\x75",serbuffer);
-
-        Disable();
-
-        TOSERIAL(serbuffer);
-
-        Enable();
+        if((str=AllocVec(512,MEMF_CLEAR))!=0) {
+          dest=str;
+          args=(ULONG *)&fmt;
+          args++;
+    
+          RawDoFmt((char *)fmt,args,(void (*)())"\x16\xC0\x4E\x75",dest);
+          kprintf("%s",dest);
+        }
       }
     }
-
-  #endif
-
-
-#else
-
-void xkprintf(ULONG type,const char *fmt, ... ) {
-  UBYTE *str;
-  UBYTE *dest;
-  ULONG *args;
-  ULONG debug=mask_debug;
-  ULONG debugdetailed=mask_debug & ~(DEBUG_CACHEBUFFER|DEBUG_NODES|DEBUG_LOCK|DEBUG_BITMAP);
-
-  if((debugdetailed & type)!=0 || ((type & 1)==0 && (debug & type)!=0)) {
-    if((str=AllocVec(512,MEMF_CLEAR))!=0) {
-      dest=str;
-      args=(ULONG *)&fmt;
-      args++;
-
-      RawDoFmt((char *)fmt,args,(void (*)())"\x16\xC0\x4E\x75",dest);
-      kprintf("%s",dest);
-    }
-  }
-}
-
+    
+    #endif
 #endif
 
 #endif
